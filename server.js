1d91623bf5cc4c3d4e485fc61344c4ba3808216b
Pedro <pedro@tridata.com.br>
2025-11-17 10:37:19 -0300

    chore: importar cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import reuniaoRoutes from './routes/reuniaoRoutes.js';
 import { connectDB } from "./utils/database.js";
 
 const server = express();
+server.use(cors());
 
 server.use(express.json());
 server.use(express.urlencoded({extended: false}));
