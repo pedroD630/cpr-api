7e459c5826ff65dc43c9995133ebff53bc5cddd5
Pedro <pedro@tridata.com.br>
2025-11-27 07:57:39 -0300

    feat: inserir rota PUT atualizar dados reuniao

--- routes/reuniaoRoutes.js
+++ routes/reuniaoRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {inserirReuniao, deletarReuniao} from '../controllers/reuniaoController.js';
+import {inserirReuniao, atualizarReuniao, deletarReuniao} from '../controllers/reuniaoController.js';
 
 const router = express.Router();
 
 router.post('/', inserirReuniao);
+router.put('/:id', atualizarReuniao);
 router.delete('/:id', deletarReuniao);
 
 export default router;
