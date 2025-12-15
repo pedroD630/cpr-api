76b1187a6612b654285159d2ab9dd22ef70929b7
Pedro <pedro@tridata.com.br>
2025-12-15 14:54:05 -0300

    chore: habilitar parser urlencoded extented

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = express();
 server.use(cors());
 
 server.use(express.json());
-server.use(express.urlencoded({extended: false}));
+server.use(express.urlencoded({extended: true}));
 
 server.use('/api/reuniao', reuniaoRoutes);
 
